5948fefcab9cf4ef36b3a7469cb8f605e2d47d71
Nathan <antnefedov33@gmail.com>
2023-12-13 19:31:58 +0300

    added smooth scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,11 @@
 "use strict"
 
 window.addEventListener('scroll', (e) => {
-    document.body.style.cssText = `--scroll-top: ${this.scrollY}px`
+    document.body.style.cssText += `--scroll-top: ${this.scrollY}px`
+});
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
 })
